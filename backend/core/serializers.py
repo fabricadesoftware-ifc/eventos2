5a19d025b7b04c1519259d0226dd50bd6b3e6a9f
yurihs <yurisalvador@hotmail.com>
2019-10-17 14:50:19 -0300

    Add user/current_user endpoints

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,8 +1,32 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
-from core.models import Event, Sponsorship, SponsorshipCategory
+from core.models import Event, Sponsorship, SponsorshipCategory, User
 from images.models import Image
 from images.serializers import ImageSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name"]
+        read_only_fields = ["email"]
+
+
+class UserCreateSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField(max_length=255, write_only=True)
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=150)
+
+    def validate_password(self, value):
+        if validate_password(value) is not None:
+            raise serializers.ValidationError("Invalid password")
+        return make_password(value)
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data, username=validated_data["email"])
 
 
 class SponsorshipCategorySerializer(serializers.ModelSerializer):
