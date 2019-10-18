3b8896f8e71df3e13fbf8903bdedcc9517356716
yurihs <yurisalvador@hotmail.com>
2019-10-18 15:55:35 -0300

    Add username to REQUIRED_FIELDS so createsuperuser command works

--- backend/eventos2/core/models/user.py
+++ backend/eventos2/core/models/user.py
@@ -6,4 +6,4 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
