d625e41fa186513f2804e7b1c38fd2298a4ffa84
yurihs <yurisalvador@hotmail.com>
2020-01-08 16:11:43 -0300

    Add user integration tests

---
+++ backend/tests/integration/test_user.py
@@ -0,0 +1,112 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from eventos2.core.models import User
+
+
+@pytest.mark.django_db
+def test_create_valid(api_client):
+    # DADO dados válidos para um user.
+    # QUANDO a API é chamada para criar o user.
+    resp = api_client.post(
+        reverse("user-list"),
+        {
+            "email": "user@example.com",
+            "password": "an-example-password",
+            "first_name": "User",
+            "last_name": "Example",
+        },
+    )
+
+    # ENTÃO a resposta de sucesso deve conter os dados do usuário.
+    assert resp.status_code == status.HTTP_201_CREATED
+    assert resp.data["email"] == "user@example.com"
+
+    # E ENTÃO o usuário deve existir no banco.
+    assert User.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_retrieve_current_valid(api_client, user_factory):
+    # DADO um user autenticado.
+    user = user_factory(name="user", permissions=[])
+    api_client.force_authenticate(user=user)
+
+    # QUANDO a API é chamada para obter o user atual.
+    resp = api_client.get(reverse("user-current"))
+
+    # ENTÃO a resposta deve ser de sucesso.
+    assert resp.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_retrieve_current_invalid(api_client, user_factory):
+    # DADO nenhum user autenticado.
+
+    # QUANDO a API é chamada para obter o user atual.
+    resp = api_client.get(reverse("user-current"))
+
+    # ENTÃO a resposta deve ser falta de permissão
+    assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_update_current_valid(api_client, user_factory):
+    # DADO um user autenticado.
+    user = user_factory(name="user", permissions=[])
+    api_client.force_authenticate(user=user)
+
+    # QUANDO a API é chamada para editar o user atual.
+    resp = api_client.put(
+        reverse("user-current"),
+        {"first_name": "New first name", "last_name": "New last name"},
+    )
+
+    # ENTÃO a resposta de sucesso deve conter o user modificado.
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.data["first_name"] == "New first name"
+
+    # E ENTÃO o user deve ser modificado no banco.
+    assert User.objects.get(pk=user.pk).first_name == "New first name"
+
+
+@pytest.mark.django_db
+def test_update_current_invalid(api_client):
+    # DADO nenhum user autenticado.
+
+    # QUANDO a API é chamada para editar o user atual.
+    resp = api_client.put(
+        reverse("user-current"),
+        {"first_name": "New first name", "last_name": "New last name"},
+    )
+
+    # ENTÃO a resposta deve ser falta de permissão
+    assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_delete_current_valid(api_client, user_factory):
+    # DADO um user autenticado.
+    user = user_factory(name="user", permissions=[])
+    api_client.force_authenticate(user=user)
+
+    # QUANDO a API é chamada para remover o user atual.
+    resp = api_client.delete(reverse("user-current"))
+
+    # ENTÃO a resposta deve ser de sucesso.
+    assert resp.status_code == status.HTTP_204_NO_CONTENT
+
+    # E ENTÃO o user deve ser removido do banco.
+    assert User.objects.filter(pk=user.pk).exists() is False
+
+
+@pytest.mark.django_db
+def test_delete_current_invalid(api_client):
+    # DADO nenhum user autenticado.
+
+    # QUANDO a API é chamada para remover o user atual.
+    resp = api_client.delete(reverse("user-current"))
+
+    # ENTÃO a resposta deve ser falta de permissão
+    assert resp.status_code == status.HTTP_403_FORBIDDEN
