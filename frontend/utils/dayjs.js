84c107a6e72a43a7a67b9c98f1c7e374e47afabc
yurihs <yurisalvador@hotmail.com>
2020-07-31 21:51:29 -0300

    frontend: add admin activity management page

--- frontend/utils/dayjs.js
+++ frontend/utils/dayjs.js
@@ -2,8 +2,10 @@ import dayjs from 'dayjs'
 import en from 'dayjs/locale/en'
 import ptBR from 'dayjs/locale/pt-br'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
+import isBetween from 'dayjs/plugin/isBetween'
 
 dayjs.extend(LocalizedFormat)
+dayjs.extend(isBetween)
 
 // Renomear locales para espelhar a configuração do nuxt
 dayjs.locale('en', en)
