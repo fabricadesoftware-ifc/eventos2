5a88ff9776a4433f7db387c2950cb26cdea98261
yurihs <yurisalvador@hotmail.com>
2020-08-14 13:45:22 -0300

    backend: serve staticfiles with gunicorn when running in DEBUG mode

--- eventos2/urls.py
+++ eventos2/urls.py
@@ -42,3 +42,4 @@ if settings.DEBUG:  # pragma: no cover - debugging
 # Servir arquivos de media. Só será efetivo quando rodando no modo debug.
 # Em produção, o nginx é quem serve estes arquivos.
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
