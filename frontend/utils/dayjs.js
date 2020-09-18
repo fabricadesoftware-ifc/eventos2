18c5510a5279cabd97c75c6677c8b81b75c56e0a
yurihs <yurisalvador@hotmail.com>
2020-09-18 13:38:33 -0300

    frontend: Change day.js english locale to enCA; the default one doesn't define locale formats

--- frontend/utils/dayjs.js
+++ frontend/utils/dayjs.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import en from 'dayjs/locale/en'
+import enCA from 'dayjs/locale/en-ca'
 import ptBR from 'dayjs/locale/pt-br'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import isBetween from 'dayjs/plugin/isBetween'
@@ -8,7 +8,7 @@ dayjs.extend(LocalizedFormat)
 dayjs.extend(isBetween)
 
 // Renomear locales para espelhar a configuração do nuxt
-dayjs.locale('en', en)
+dayjs.locale('en', enCA)
 dayjs.locale('pt', ptBR)
 
 export default dayjs
