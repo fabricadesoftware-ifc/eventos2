91e912c9ac4395fee148008c7b581b58e21b251b
yurihs <yurisalvador@hotmail.com>
2020-10-09 18:28:07 -0300

    frontend: better generic error handling

--- frontend/mixins/errorMixin.js
+++ frontend/mixins/errorMixin.js
@@ -8,21 +8,33 @@ function getSingleFieldError(error) {
   }
 }
 function showError(message, error) {
+  // Se existem erros em campos, e um form na página,
+  // mostrar os erros junto a cada campo.
+  if (this.$refs.form && error.fields) {
+    // Também mostrar a mensagem geral
+    if (this.error !== undefined) {
+      this.error = message
+    }
+    this.$refs.form.setErrors(error.fields)
+    return
+  }
+
+  // Se existe só um campo com erro, usá-lo como mensagem de erro.
+  message = getSingleFieldError(error) || message
+
+  // Se o componente tem uma variável error, mostrar o erro nela.
   if (this.error !== undefined) {
     this.error = message
+    return
   }
-  if (this.$refs.form && error.fields) {
-    this.$refs.form.setErrors(error.fields)
-  } else {
-    // Se existe só um campo com erro, usá-lo como mensagem de erro.
-    message = getSingleFieldError(error) || message
-    this.$buefy.toast.open({
-      duration: 5000,
-      message,
-      position: 'is-bottom-right',
-      type: 'is-danger'
-    })
-  }
+
+  // A ultima alternativa é mostrar um toast com o erro.
+  this.$buefy.toast.open({
+    duration: 5000,
+    message,
+    position: 'is-bottom-right',
+    type: 'is-danger'
+  })
 }
 
 export default {
